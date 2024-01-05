8fb09c4b4bb4a7c3cf267099afdd12fe8605df66
Luis Reyes <yuiftw05@gmail.com>
2024-01-05 12:59:44 -0600

    component: first two pictures from projects component with layout

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,13 +1,28 @@
 
 import styles from './Projects.module.css'
 
+//Projects images
+import EngPartner from '../../assets/ProjectsPictures/image.png'
+import dogsAppPicture from '../../assets/ProjectsPictures/dogsApp.png'
+
 const Projects = ()=>{
     return(
         <div className={styles.projectsMainContainer}>
             <div className={styles.titleAndContactBtnContainer}>
                 <h1>Projects</h1>
                 <button>Contact me</button>
+
             </div>
+                
+                <div className={styles.projectsContainer}>
+                    <figure className={styles.projectContainer}>
+                        <img src={EngPartner} alt="EngPartner app picture" />
+                    </figure>
+
+                    <figure className={styles.projectContainer}>
+                        <img src={dogsAppPicture} alt="Dogs app picture" />
+                    </figure>
+                </div>
         </div>
     )
 }

1b196b1b2e7aa687eb122abb638166c8616beb65
Luis Reyes <yuiftw05@gmail.com>
2024-01-06 22:56:11 -0600

    Project component: adding links to projects and their deplor(rick and morty and dogs app pending(deploy))

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -7,6 +7,20 @@ import dogsAppPicture from '../../assets/ProjectsPictures/dogsApp.png'
 import rickandmoryAppPicture from '../../assets/ProjectsPictures/rickAndMortyApp.png'
 import rockScissorsPaperGameAppPicture from '../../assets/ProjectsPictures/RockScissorsAndPapersGameProject.png'
 
+const EngPartnerUrl = "https://github.com/Luuiskame/EngPartnerV2"
+const EngPartnerDeploy = "https://engpartner-aa0ce.web.app/"
+
+const rockGameUrl = "https://github.com/Luuiskame/rock-paper-scissors-game"
+const rockGameDeploy = "https://luuiskame.github.io/rock-paper-scissors-game/"
+
+const dogsAppUrl = "https://github.com/Luuiskame/DogsAppv3"
+const dogsAppDeploy = ""
+
+const rickUrl = "https://github.com/Luuiskame/RickAndMortyApp"
+const rickDeploy = ""
+
+
+
 const Projects = ()=>{
     return(
         <div className={styles.projectsMainContainer}>
@@ -23,8 +37,8 @@ const Projects = ()=>{
 
                         <h3>Chat app with real time translation</h3>
                         <div className={styles.projectBtnsContainer}>
-                        <button>View Project</button>
-                        <button>View Code</button>
+                        <button><a href={EngPartnerDeploy} target='_blank'>View Project</a></button>
+                        <button><a href={EngPartnerUrl} target='_blank'>View Code</a></button>
                         </div>
                     </figure>
 
@@ -33,8 +47,8 @@ const Projects = ()=>{
 
                         <h3>Dogs searcher app</h3>
                         <div className={styles.projectBtnsContainer}>
-                        <button>View Project</button>
-                        <button>View Code</button>
+                        <button><a href={dogsAppDeploy} target='_blank'>View Project</a></button>
+                        <button><a href={dogsAppUrl} target='_blank'>View Code</a></button>
                         </div>
                     </figure>
 
@@ -43,8 +57,8 @@ const Projects = ()=>{
 
                         <h3>Rick And Morty searcher app</h3>
                         <div className={styles.projectBtnsContainer}>
-                        <button>View Project</button>
-                        <button>View Code</button>
+                        <button><a href={rickDeploy} target='_blank'>View Project</a></button>
+                        <button><a href={rickUrl} target='_blank'>View Code</a></button>
                         </div>
                     </figure>
 
@@ -53,8 +67,8 @@ const Projects = ()=>{
 
                         <h3>Rock Paper Scissors Game (vanilla js)</h3>
                         <div className={styles.projectBtnsContainer}>
-                        <button>View Project</button>
-                        <button>View Code</button>
+                        <button><a href={rockGameDeploy} target='_blank'>View Project</a></button>
+                        <button><a href={rockGameUrl} target='_blank'>View Code</a></button>
                         </div>
                     </figure>
                 </div>

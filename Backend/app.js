1e7367340d5bb7cea74f9b1bfbdee83c5a099d0c
Luis Reyes <yuiftw05@gmail.com>
2024-01-07 18:56:50 -0600

    controller: starting controller. vercel.json: index.js instead of app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/index.js');
+const routes = require('./routes/index');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
---
+++ Backend/controllers/postEmail.js
@@ -0,0 +1,8 @@
+require('dotenv').config
+const nodemailer = require('nodemailer')
+
+const postEmail = (req,res)=>{
+
+}
+
+module.exports = postEmail

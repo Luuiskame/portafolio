d5c564acbac57dd5640357bc1dcd1f19cd9fb09e
Luis Reyes <yuiftw05@gmail.com>
2024-09-26 10:40:48 -0600

    feat: adding an array of obj to render crucial info, starting styles of it

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,81 +1,101 @@
-
-import styles from './Projects.module.css'
+import styles from "./Projects.module.css";
 
 //Projects images
-import EngPartner from '../../assets/ProjectsPictures/image.png'
-import dogsAppPicture from '../../assets/ProjectsPictures/dogsappv3.webp'
-import rickandmoryAppPicture from '../../assets/ProjectsPictures/rickAndMortyApp.png'
-import rockScissorsPaperGameAppPicture from '../../assets/ProjectsPictures/RockScissorsAndPapersGameProject.png'
+import EngPartner from "../../assets/ProjectsPictures/image.png";
+import dogsAppPicture from "../../assets/ProjectsPictures/dogsappv3.webp";
+import rickandmoryAppPicture from "../../assets/ProjectsPictures/rickAndMortyApp.png";
+import rockScissorsPaperGameAppPicture from "../../assets/ProjectsPictures/RockScissorsAndPapersGameProject.png";
+import pawpalPicture from "../../assets/ProjectsPictures/pawpal.webp";
 
-const EngPartnerUrl = "https://github.com/Luuiskame/EngPartnerV2"
-const EngPartnerDeploy = "https://engpartner-aa0ce.web.app/"
+const projects = [
+  {
+    name: "Pawpal Community",
+    githubUrl: "https://github.com/Pawpal-community/Pawpal-web",
+    deployUrl: "https://pawpalcommunity.vercel.app/login",
+    picture: pawpalPicture,
+    description:
+      "Pawpal is a social community platform for pet lovers, and its full responsive!",
+      welcomeAccount: { email: "akaluis91@gmail.com", password: "hola12345" },
+  },
+  {
+    name: "EngPartner",
+    githubUrl: "https://github.com/Luuiskame/EngPartnerV2",
+    deployUrl: "https://engpartner-aa0ce.web.app/",
+    picture: EngPartner,
+    description:
+      "Platform designed to connect language learners. It features real-time chat translation",
+    welcomeAccount: { email: "akaluis91@gmail.com", password: "hola12345" },
+  },
+  // {
+  //   name: "Rock Paper Scissors Game",
+  //   githubUrl: "https://github.com/Luuiskame/rock-paper-scissors-game",
+  //   deployUrl: "https://luuiskame.github.io/rock-paper-scissors-game/",
+  //   picture: rockScissorsPaperGameAppPicture,
+  //   description: "A simple Rock-Paper-Scissors game built using web technologies, where users can play against the computer with a modern UI."
+  // },
+  {
+    name: "Dogs App",
+    githubUrl: "https://github.com/Luuiskame/DogsAppv3",
+    deployUrl: "https://dogs-appv3-r3yj.vercel.app/",
+    picture: dogsAppPicture,
+    description:
+      "Dogs App allows users to search, filter, and view detailed information about various dog breeds.",
+  },
+  {
+    name: "Rick and Morty App",
+    githubUrl: "https://github.com/Luuiskame/RickAndMortyApp",
+    deployUrl: "https://rick-and-morty-app-frontend.vercel.app/",
+    picture: rickandmoryAppPicture,
+    description:
+      "App that fetches and displays character information with filtering, favorites and searching features.",
+  },
+];
 
-const rockGameUrl = "https://github.com/Luuiskame/rock-paper-scissors-game"
-const rockGameDeploy = "https://luuiskame.github.io/rock-paper-scissors-game/"
+const Projects = () => {
+  return (
+    <div className={styles.projectsMainContainer}>
+      <div className={styles.titleAndContactBtnContainer}>
+        <h1>Projects</h1>
+        <a href="#footer">
+          <button>Contact me</button>
+        </a>
+      </div>
 
-const dogsAppUrl = "https://github.com/Luuiskame/DogsAppv3"
-const dogsAppDeploy = "https://dogs-appv3-r3yj.vercel.app/"
+      <div className={styles.projectsContainer}>
+        {projects.map((project) => (
+          <figure key={project.name} className={styles.projectContainer}>
+            <img src={project.picture} alt="EngPartner app picture" />
 
-const rickUrl = "https://github.com/Luuiskame/RickAndMortyApp"
-const rickDeploy = "https://rick-and-morty-app-frontend.vercel.app/"
-
-
-
-const Projects = ()=>{
-    return(
-        <div className={styles.projectsMainContainer}>
-            <div className={styles.titleAndContactBtnContainer}>
-                <h1>Projects</h1>
-                <a href="#footer">
-                <button>Contact me</button>
-                </a>
-
-            </div>
-                
-                <div className={styles.projectsContainer}>
-                    
-                    <figure className={styles.projectContainer}>
-                        <img src={EngPartner} alt="EngPartner app picture" />
-
-                        <h3>Chat app with real time translation</h3>
-                        <div className={styles.projectBtnsContainer}>
-                        <button><a href={EngPartnerDeploy} target='_blank'>View Project</a></button>
-                        <button><a href={EngPartnerUrl} target='_blank'>View Code</a></button>
-                        </div>
-                    </figure>
-
-                    <figure className={styles.projectContainer}>
-                        <img src={dogsAppPicture} alt="Dogs app picture" />
-
-                        <h3>Dogs searcher app</h3>
-                        <div className={styles.projectBtnsContainer}>
-                        <button><a href={dogsAppDeploy} target='_blank'>View Project</a></button>
-                        <button><a href={dogsAppUrl} target='_blank'>View Code</a></button>
-                        </div>
-                    </figure>
-
-                    <figure className={styles.projectContainer}>
-                        <img src={rickandmoryAppPicture} alt="Dogs app picture" />
-
-                        <h3>Rick And Morty searcher app</h3>
-                        <div className={styles.projectBtnsContainer}>
-                        <button><a href={rickDeploy} target='_blank'>View Project</a></button>
-                        <button><a href={rickUrl} target='_blank'>View Code</a></button>
-                        </div>
-                    </figure>
-
-                    <figure className={styles.projectContainer}>
-                        <img src={rockScissorsPaperGameAppPicture} alt="Dogs app picture" />
-
-                        <h3>Rock Paper Scissors Game (vanilla js)</h3>
-                        <div className={styles.projectBtnsContainer}>
-                        <button><a href={rockGameDeploy} target='_blank'>View Project</a></button>
-                        <button><a href={rockGameUrl} target='_blank'>View Code</a></button>
-                        </div>
-                    </figure>
+            <h3>{project.description}</h3>
+            {project.welcomeAccount && (
+              <div className={styles.welcomeAccount}>
+                <p>
+                  You can register, but also feel free to use this welcome
+                  account:
+                </p>
+                <div className={styles.dataContainer}>
+                <p>Email: {project.welcomeAccount.email}</p>
+                <p>Password: {project.welcomeAccount.password}</p>
                 </div>
-        </div>
-    )
-}
+              </div>
+            )}
+            <div className={styles.projectBtnsContainer}>
+              <button>
+                <a href={project.deployUrl} target="_blank">
+                  View Project
+                </a>
+              </button>
+              <button>
+                <a href={project.githubUrl} target="_blank">
+                  View Code
+                </a>
+              </button>
+            </div>
+          </figure>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;

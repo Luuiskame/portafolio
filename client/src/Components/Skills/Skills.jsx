eff58fbfbcf14df31981ce45a0358df710ad05aa
Luis Reyes <yuiftw05@gmail.com>
2025-05-13 00:16:05 -0600

    feat: skills section updated

--- client/src/Components/Skills/Skills.jsx
+++ client/src/Components/Skills/Skills.jsx
@@ -4,21 +4,25 @@ const Skills = ()=>{
     return(
         <div className={styles.skillsContainer}>
             <div className={styles.frontendSkills}>
-                <h2>HTML</h2>
-                <h2>CSS</h2>
-                <h2>Javascript</h2>
                 <h2>React</h2>
-                <h2>Redux</h2>
-                <h2>Github</h2>
+                <h2>NextJs</h2>
+                <h2>CSS</h2>
+                <h2>Tailwind</h2>
+                <h2>Typescript</h2>
+                <h2>Redux toolkit</h2>
+                <h2>Astro</h2>
                 <h2>Git</h2>
             </div>
 
             <div className={styles.backendAndDatabaseSkills}>
                 <h2>Node.js</h2>
                 <h2>Express</h2>
+                <h2>Web sockets</h2>
+                <h2>Supabase</h2>
                 <h2>Sequelize</h2>
                 <h2>PostgreSQL</h2>
                 <h2>Firebase</h2>
+                <h2>Redis</h2>
             </div>
 
             <div className={styles.bottomLine}></div>
